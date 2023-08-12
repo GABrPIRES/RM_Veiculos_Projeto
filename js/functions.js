bf43a62874fba86766943a73a4ef9790cb26f2fc
GABrPIRES <gabrielribeiropires@outlook.com>
2023-08-11 23:53:26 -0300

    Inicializamos o projeto

---
+++ js/functions.js
@@ -0,0 +1,87 @@
+$(function () {
+
+    var amt = $('.slide-testimonials-1').length;
+    console.log(amt)
+    var curIndex = 0;
+    var delay = 3000;
+
+    autoPlay();
+
+    function autoPlay() {
+        interval = setInterval(function () {
+            curIndex++;
+            if (curIndex == amt) {
+                curIndex = 0;
+            }
+            goToSlider(curIndex);
+        }, delay)
+    }
+
+    function goToSlider(curIndex) {
+        var offSetX = $('.slide-testimonials-1').eq(curIndex).offset().left - $('.slide-testimonials-1').offset().left;
+
+        $('.slide-testimonials').stop().animate({ 'scrollLeft': offSetX + 'px' });
+    }
+
+    /*
+
+    $('#btn-left').click(function () {
+        curIndex--;
+        if (curIndex == amt) {
+            curIndex = 0;
+        }
+        clearInterval(interval);
+        goToSlider(curIndex);
+        setTimeout(autoPlay, 500);
+    })
+
+    $('#btn-right').click(function () {
+        curIndex++;
+        if (curIndex == amt) {
+            curIndex = 0;
+        }
+        clearInterval(interval);
+        goToSlider(curIndex);
+        setTimeout(autoPlay, 500);
+    })
+    */
+
+    $('#btn-left').click(function () {
+        curIndex--;
+        if (curIndex < 0) {
+            curIndex = amt - 1;
+        }
+        // Verificar se o intervalo já está em execução
+        if (!interval) {
+            // Iniciar o auto-play novamente após um breve intervalo
+            interval = setInterval(function () {
+                curIndex++;
+                if (curIndex == amt) {
+                    curIndex = 0;
+                }
+                goToSlider(curIndex);
+            }, delay);
+        }
+        goToSlider(curIndex);
+    });
+
+    $('#btn-right').click(function () {
+        curIndex++;
+        if (curIndex == amt) {
+            curIndex = 0;
+        }
+        // Verificar se o intervalo já está em execução
+        if (!interval) {
+            // Iniciar o auto-play novamente após um breve intervalo
+            interval = setInterval(function () {
+                curIndex++;
+                if (curIndex == amt) {
+                    curIndex = 0;
+                }
+                goToSlider(curIndex);
+            }, delay);
+        }
+        goToSlider(curIndex);
+    });
+
+})
